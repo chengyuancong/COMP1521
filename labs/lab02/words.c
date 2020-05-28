c05977451d2155c7e3aab6c66ee6e46712f45d30
Yuancong <chengyuantsung@gmail.com>
2020-05-28 15:19:01 +1000

    Marked lab02

--- labs/lab02/words.c
+++ labs/lab02/words.c
@@ -1,7 +1,5 @@
-/*
-   Lab - Debugging with gdb
-   File: words.c
-*/
+// Lab - Debugging with gdb
+// File: words.c
 
 // Include files
 #include <ctype.h>
@@ -10,7 +8,7 @@
 #include <string.h>
 
 
-//Constants
+// Constants
 #define LINELENGTH    100
 
 
@@ -21,77 +19,68 @@ int  mark_words(char* line, char* words[]);
 
 
 // Main Function
-int main( void )
-{
-  char *line; 
-  char *words[51];
-  char *rwords[51];
-  int   count;
+int main(void) {
+    char *line; 
+    char *words[51];
+    char *rwords[51];
+    int   count;
 
-  if(( line = (char*) malloc(LINELENGTH * sizeof(char))) == NULL ) {
-    return 1;
-  }
-  strcpy(line, "this   is a sample  line to      be broken into words.");
+    if ((line = (char *) malloc(LINELENGTH * sizeof(char))) == NULL) {
+        return 1;
+    }
+    strcpy(line, "this   is a sample  line to      be broken into words.");
 
-  count = mark_words( line, words );
+    count = mark_words(line, words);
     
-  reverse_words( words, rwords, count );
+    reverse_words(words, rwords, count);
 
-  print_words( rwords );
+    print_words(rwords);
 
-  return 0;
+    return 0;
 }
 
 /*
-   Converts line to a packed string of words and puts
-   pointers to each word in words[].
+Converts line to a packed string of words and puts
+pointers to each word in words[].
   
-   returns the number of words it found.
+returns the number of words it found.
 */
-int mark_words( char* line, char* words[] )
-{
-  int i, count = 1;
-  char inbetween = 0;
-
-  words[0] = line;
-  for( i=0; line[i] != 0 ; ++i ) {
-    if( isspace( line[i] )) {
-      inbetween = 1;
-      line[i] = '\0';
+int mark_words(char* line, char* words[]) {
+    int i, count = 1;
+    char inbetween = 0;
+    words[0] = line;
+    for(i = 0; line[i] != 0; i++) {
+        if (isspace(line[i])) {
+            inbetween = 1;
+            line[i] = '\0';
+        } else {
+            if (inbetween) {
+	            inbetween = 0;
+	            words[count++] = line + i;
+            }
+        }
     }
-    else { // i.e. line[i] is not a whitespace character
-      if( inbetween ) {
-	inbetween = 0;
-	words[count++] = line + i;
-      }
-    }
-  }
-  words[count] = NULL;
-  return count;
+    words[count] = NULL;
+    return count;
 }
 
 /*
-   Copies the pointers in words[] to rwords[] in reverse order.
-   count is the number of pointers in words[]
+Copies the pointers in words[] to rwords[] in reverse order.
+count is the number of pointers in words[]
 */
-void reverse_words( char* words[], char* rwords[], int count )
-{
-  int j = 0;
-
-  for( j = 0; words[j] != NULL; j++ ) {
-    rwords[j] = words[count-j-1];
-  }
-  rwords[j] = '\0';
+void reverse_words(char* words[], char* rwords[], int count) {
+    int j = 0;
+    for(j = 0; words[j] != NULL; j++) {
+        rwords[j] = words[count-j-1];
+    }
+    rwords[j] = '\0';
 }
 
-/*
-   Prints each word in words[] to stdout in order and preceded by its number.
-*/
-void print_words( char* words[] )
-{
-  int k;
 
-  for( k=0; words[k] != 1; k++ ) {
-    printf("%2d. %s\n", k+1, words[k]);
-  }
+// Prints each word in words[] to stdout in order and preceded by its number.
+void print_words(char* words[]) {
+    int k;
+    for (k=0; words[k] != NULL; k++) {
+        printf("%2d. %s\n", k+1, words[k]);
+    }
 }

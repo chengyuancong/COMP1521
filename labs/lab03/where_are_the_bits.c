5ac2a9101572f736df026d0fda8977d4399b9f19
Yuancong <chengyuantsung@gmail.com>
2020-05-29 22:55:18 +1000

    Finished exercise1

--- labs/lab03/where_are_the_bits.c
+++ labs/lab03/where_are_the_bits.c
@@ -1,6 +1,6 @@
 // where_are_the_bits.c ... determine bit-field order
 // CP1521 Lab 03 Exercise
-// Written by ...
+// Written by Yuancong
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -14,8 +14,15 @@ struct _bit_fields {
 int main(void)
 {
    struct _bit_fields x;
+   x.a = 0x1;
+   x.b = 0x22;
+   x.c = 0x33333;
 
    printf("%lu\n",sizeof(x));
-
+   char * p = (char *)&x;
+   for (int i = 0; i < sizeof(x); i++) {
+      printf("%x ", *p);
+      p++;
+   }
    return 0;
 }

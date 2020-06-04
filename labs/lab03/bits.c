9bf82ff7961f151d429071d788dfa47925595a7d
chengyuancong <chengyuantsung@gmail.com>
2020-06-04 13:01:28 +1000

    Marked lab03

--- labs/lab03/bits.c
+++ labs/lab03/bits.c
@@ -14,35 +14,35 @@ int main()
 
 	// Code to generate and display the largest "unsigned int" value
 	unsigned int y = 0;
-	for (unsigned int i = 0; i < 8 * sizeof y; i++) {
+	for (int i = 0; i < 8 * sizeof y; i++) {
 		y |= (unsigned int) 1 << i;
 	}
 	printf("unsigned int %x, %u\n", y, y);
 
 	// Code to generate and display the largest "long int" value
 	long int xx = 0;
-	for (long int i = 0; i < (8 * sizeof xx) - 1; i++) {
+	for (int i = 0; i < (8 * sizeof xx) - 1; i++) {
 		xx |= (long int) 1 << i;
 	}
 	printf("long int %lx, %ld\n", xx, xx);
 
 	// Code to generate and display the largest "unsigned long int" value
 	unsigned long int xy = 0;
-	for (unsigned long int i = 0; i < 8 * sizeof xy; i++) {
+	for (int i = 0; i < 8 * sizeof xy; i++) {
 		xy |= (unsigned long int) 1 << i;
 	}
 	printf("unsigned long int %lx, %lu\n", xy, xy);
 
 	// Code to generate and display the largest "long long int" value
 	long long int xxx = 0;
-	for (long long int i = 0; i < (8 * sizeof xxx) - 1; i++) {
+	for (int i = 0; i < (8 * sizeof xxx) - 1; i++) {
 		xxx |= (long long int) 1 << i;
 	}
 	printf("long long int %llx, %lld\n", xxx, xxx);
 
 	// Code to generate and display the largest "unsigned long long int" value
 	unsigned long long int xxy = 0;
-	for (unsigned long long int i = 0; i < 8 * sizeof xxy; i++) {
+	for (int i = 0; i < 8 * sizeof xxy; i++) {
 		xxy |= (unsigned long long int) 1 << i;
 	}
 	printf("unsigned long long int %llx, %llu\n", xxy, xxy);
--- labs/lab03/where_are_the_bits.c
+++ labs/lab03/where_are_the_bits.c
@@ -11,40 +11,31 @@ struct _bit_fields {
                 c : 20;
 };
 
-union _bits32 {
-   uint32_t xval;
-   struct _bit_fields bits;
-};
-
 int main(void)
 {
    struct _bit_fields x;
-   x.a = 0b1;
-   x.b = 0b1;
-   x.c = 0b1;
+   x.a = 1;
+   x.b = 1;
+   x.c = 1;
 
-   union _bits32 u;
-   u.bits = x;
+   printf("\nSize of x is %lu bytes.\n", sizeof(x));
 
-   printf("Size of x is %lu bytes.\n",sizeof(x));
-   
-   printf("\nc: 00000000000000000001 b: 00000001 a: 0001\n");
-
-   printf("\nPrint abc by uint32_t:\n");
-   for (int i = 0; i < 32; i++) {
-      printf("%d", (u.xval >> (31 - i)) & 1);
+   printf("\nPrint abc as an int:\n");
+   int * ip = (int *) &x;
+   for (int k = 31; k >= 0; k--) {
+      printf("%d", (*ip >> k) & 1);
    }
 
    printf("\n\nPrint abc by bytes:\n");
-   char * p = (char *) &x;
+   char * cp = (char *) &x;
    for (int i = 0; i < sizeof(x); i++) {
       for (int j = 7; j >= 0; j--) {
-         printf("%d", (*p >> j) & 1);
+         printf("%d", (*cp >> j) & 1);
       }
       printf(" ");
-      p++;
+      cp++;
    }
    printf("\n");
-
    return 0;
 }
+

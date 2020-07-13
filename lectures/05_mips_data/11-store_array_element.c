3d2e21cdc23bdfb23fc5241964efd54d4299fd61
Yuancong <chengyuantsung@gmail.com>
2020-07-13 14:57:13 +1000

    Delete lecture examples

--- lectures/05_mips_data/11-store_array_element.c
+++
@@ -1,9 +0,0 @@
-// simple example of accessing an array element
-
-#include <stdio.h>
-
-int x[10];
-
-int main(void) {
-    x[3] = 17;
-}
--- lectures/05_mips_data/9-unalign.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdint.h>
-
-int main(void) {
-    uint8_t bytes[32];
-    uint32_t *i = (int *)bytes[1];
-    *i = 0x03040506;   // store will not be aligned on a 4-byte boundary
-    printf("%d\n", bytes[1]);
-}
